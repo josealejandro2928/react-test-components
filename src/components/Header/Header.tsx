9cd02e80a933591bd753531ec24fc6d1befa289c
Jose Alejandro Concepcion Alvarez <jalejandroc2928@gmail.com>
2021-09-20 18:00:27 -0400

    fix: Put header as fixed

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,18 +5,21 @@ import './Header.scss';
 
 function Header({ links = [] }: { links?: any[] }): JSX.Element {
   return (
-    <div className="Header">
-      <div className="section" style={{ flex: '1 0 100%', maxWidth: '40%' }}>
-        <img className="logo" src={logo} alt="logo" />
-        <span className="title">Testing React Components</span>
-      </div>
+    <>
+      <div className="Header">
+        <div className="section" style={{ flex: '1 0 100%', maxWidth: '40%' }}>
+          <img className="logo" src={logo} alt="logo" />
+          <span className="title">Testing React Components</span>
+        </div>
 
-      <div className="section" style={{ flex: '1 1 100%', maxWidth: '100%', justifyContent: 'flex-end' }}>
-        {links.map((item, index) => (
-          <LinkBtn key={index} name={item.name} icon={item.icon} />
-        ))}
+        <div className="section" style={{ flex: '1 1 100%', maxWidth: '100%', justifyContent: 'flex-end' }}>
+          {links.map((item, index) => (
+            <LinkBtn key={index} name={item.name} icon={item.icon} />
+          ))}
+        </div>
       </div>
-    </div>
+      <div style={{ height: '64px' }}></div>
+    </>
   );
 }
 export default Header;
